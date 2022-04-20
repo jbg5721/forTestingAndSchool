9ebf6bcd3b7c7b046997330c40996903b22e69f6
jbg5721 <joeygardiner0@gmail.com>
2022-04-19 22:35:35 -0400

    updating python

--- textAnalysis22/Python/nlp/main.py
+++ textAnalysis22/Python/nlp/main.py
@@ -3,12 +3,23 @@ import spacy
 # nlp = spacy.cli.download("en_core_web_sm")
 nlp = spacy.load('en_core_web_sm')
 
-starSet = open('starwarsSetting.txt', 'r')
-words = starSet.read()
-wordstrings = str(words)
-print(wordstrings)
+#starSet = open('ANHroughDraft-stagedirs.txt', 'r')
+#starSet1 = open('ANHfinalDraft-stagedirs.txt', 'r')
+#words = starSet.read()
+#words1 = starSet1.read()
+#wordstrings = str(words)
+#wordstrings1 = str(words1)
+# print(wordstrings)
 
-starWords = nlp(wordstrings)
-for token in starWords:
+#starWords = nlp(wordstrings)
+#starWords1 = nlp(wordstrings1)
+# for token in starWords:
      # if not token.is_punct:
-    print(token.text, "--->", token.pos_)
+#print(wordstrings.similarity(wordstrings1))
+with open("ANHroughDraft-stagedirs.txt", "r") as f1:
+    print(f1.read())
+
+with open("ANHfinalDraft-stagedirs.txt", "r") as f2:
+    print(f2.read())
+
+print(f1.similarity(f2))
